2bb2108aaa5a6f488b6e60630107832c49ece019
cchen9299 <cchen9299@gmail.com>
2021-06-25 11:29:21 -0700

    refactor filter logic

--- src/components/CocktailCardList/index.js
+++ src/components/CocktailCardList/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import CocktailCard from '../CocktailCard';
 import { connect } from 'react-redux';
 import { fetchCocktails } from '../../actions';
@@ -10,19 +10,25 @@ function CocktailCardList({ cocktailList, fetchCocktails, searchTerm, isLoading 
     fetchCocktails();
   }, [fetchCocktails]);
 
-  const filteredCocktailList = cocktailList?.filter((drink) => {
-    if (appliedPill.length > 0) {
-      return appliedPill.every((pill) => {
-        return drink.ingredients.includes(pill);
-      });
-    } else {
-      return (
-        drink.ingredients.some((ingredient) => {
-          return ingredient?.toLowerCase().includes(searchTerm);
-        }) || drink['strDrink'].toLowerCase().includes(searchTerm)
-      );
-    }
-  });
+  const filterBySearch = (drink) => {
+    return (
+      drink.ingredients?.some((ingredient) => {
+        return ingredient?.toLowerCase().includes(searchTerm);
+      }) || drink['strDrink'].toLowerCase().includes(searchTerm)
+    );
+  };
+
+  const filterByPill = (drink) => {
+    return appliedPill?.every((pill) => {
+      return drink.ingredients.includes(pill);
+    });
+  };
+
+  const filterByAll = useMemo(() => {
+    return cocktailList.filter((drink) => {
+      return filterBySearch(drink) && filterByPill(drink);
+    });
+  }, [searchTerm, appliedPill, cocktailList]);
 
   const handlePillClickCallBack = (ingredient) => {
     appliedPill.includes(ingredient)
@@ -44,11 +50,11 @@ function CocktailCardList({ cocktailList, fetchCocktails, searchTerm, isLoading 
 
   return (
     <div style={{ display: 'flex', flexWrap: 'wrap', flex: 1, margin: '0 auto', width: '100%' }}>
-      {filteredCocktailList?.map((cocktail, index) => {
+      {filterByAll?.map((cocktail, index) => {
         return (
           <CocktailCard
             appliedPill={appliedPill}
-            key={filteredCocktailList[index]?.idDrink}
+            key={filterByAll[index]?.idDrink}
             cocktail={cocktail}
             handlePillClick={handlePillClickCallBack}
           />
@@ -64,3 +70,64 @@ const mapStateToProps = (state) => ({
 });
 
 export default connect(mapStateToProps, { fetchCocktails })(CocktailCardList);
+
+//functioning cross reference filter
+
+// const filteredCocktailList = cocktailList?.filter((drink) => {
+//   return (
+//     drink.ingredients.some((ingredient) => {
+//       return ingredient?.toLowerCase().includes(searchTerm);
+//     }) || drink['strDrink'].toLowerCase().includes(searchTerm)
+//   );
+// });
+
+// const secondFilter = filteredCocktailList?.filter((drink) => {
+//   if (appliedPill.length > 0) {
+//     return appliedPill.every((pill) => {
+//       return drink.ingredients.includes(pill);
+//     });
+//   } else return filteredCocktailList;
+// });
+
+//attempt at combining filters
+
+// const [filteredCocktailList, setFilteredCocktailList] = useState([]);
+
+// useEffect(() => {
+//   setFilteredCocktailList(cocktailList);
+// }, [isLoading]);
+
+// const filterBySearch = cocktailList?.filter((drink) => {
+//   return (
+//     drink.ingredients.some((ingredient) => {
+//       return ingredient?.toLowerCase().includes(searchTerm);
+//     }) || drink['strDrink'].toLowerCase().includes(searchTerm)
+//   );
+// });
+
+// const filterByPill = cocktailList?.filter((drink) => {
+//   if (appliedPill.length > 0) {
+//     return appliedPill.every((pill) => {
+//       return drink.ingredients.includes(pill);
+//     });
+//   }else return filterBySearch
+// });
+
+// useEffect(() => {
+//   setFilteredCocktailList(filterBySearch);
+//   setFilteredCocktailList(filterByPill);
+// }, [searchTerm, appliedPill]);
+
+// const filteredCocktailList = cocktailList?.filter((drink) => {
+//   if (appliedPill.length > 0) {
+//     return appliedPill.every((pill) => {
+//       return drink.ingredients.includes(pill);
+//     });
+//   } else {
+//     return (
+//       drink.ingredients.some((ingredient) => {
+//         return ingredient?.toLowerCase().includes(searchTerm);
+//       }) || drink['strDrink'].toLowerCase().includes(searchTerm)
+//     );
+//   }
+// });
